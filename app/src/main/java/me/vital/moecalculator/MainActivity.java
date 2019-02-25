3b4a8cc4aec9e03b6ac649ee2f1032c4c39d85a4
Bruno Rodrigues Santos <bruesmanbruesman@hotmail.com>
2019-02-25 10:54:57 -0300

    [ADD] Added layout and initialized ui components

--- app/src/main/java/me/vital/moecalculator/MainActivity.java
+++ app/src/main/java/me/vital/moecalculator/MainActivity.java
@@ -2,12 +2,23 @@ package me.vital.moecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button sum;
+    private Button subtract;
+    private EditText number_x;
+    private EditText number_y;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sum = findViewById(R.id.button_sum);
+        subtract = findViewById(R.id.button_subtract);
+
     }
 }
